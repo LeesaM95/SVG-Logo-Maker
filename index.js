4efac339beb9a6aac3b90706092600752fb44790
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2024-04-07 18:04:22 -0400

    adjusted some code to make sure it works

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
-const { writeSVGFile } = require('./lib/fileWriter');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
 
-//defining a function to store our user's input
-class SVGMaker {
-    constructor() {
-        this.userInput = {};
-        this.examplesPath = path.join(__dirname, 'examples');
-    }
-}
 
 //define the user prompts that will populate our data
-function userInput() {
-    return inquirer.prompt([
+function userPrompts() {
+    
+    inquirer
+    .prompt([
         {
             name: "text",
             type: "input",
             message: "Please enter up to three characters for your text",
-            validate: function (value) {
-                return value.length > 0 && value.length <= 3 ? true : "Please enter up to three characters"
-            }
         },
         {
             name: "textColor",
@@ -41,66 +31,45 @@ function userInput() {
             message: "Please enter a color for your shape (can be 'red' or a hexcode)"
         }
     ])
-}
-
-
-function createShape() {
-
-    // define the corresponding shape based on user data
-    let shape;
-    switch (this.userInput.shape) {
-        case 'Circle':
-            shape = new Circle();
-            break;
-        case 'Triangle':
-            shape = new Triangle();
-            break;
-        case 'Square':
-            shape = new Square();
-            break;
-        default:
-            console.error('Invalid Shape!');
-            return null;
-    }
-
-    //set the shape color...
-    shape.setColor(this.userInput.shapeColor);
-    shape.setTextColor(this.userInput.textColor);
-
-    return shape;
-
-    //generate the SVG string needed for the shape...
-}
-
-function writeSVGFile(shape) {
-    const shapeSvg = shape.render();
-    const textSvg = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.userInput.textColor}">${this.userInput.text}</text>`
-    const svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n  ${shapeSvg}\n  ${textSvg}\n</svg>`;
-
-    const fileName = `logo.svg`;
-    const filePath = path.join(this.examplesPath, fileName);
-
-    try {
-        fs.writeFileSync(filePath, svgString);
-        console.log(`Generated ${fileName} in the examples folder!`)
-    } catch (err) {
-        console.error("Oops! Something went wrong:", err.message)
-    };
-
-
-    async function  run() {
-        this.userInput = await this.userInput();
-        const shape = this.createShape();
-
-        if (shape) {
-            this.writeSVGFile(shape)
+    .then((answers) => {
+        if(answers.text.length > 3) {
+            console.log('Please enter at least three characters');
+            userPrompts();
         } else {
-            console.log("Creation failed. Please try again.")
+            writeToFile("logo.svg", answers)
         }
-    }
+    })
 };
 
 
-// Tell our app to start running.
-const logoMaker = new SVGMaker();
-logoMaker.run();
+function writeToFile(fileName, answers) {
+    let svgString = "";
+        svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+        svgString += "<g>";
+        svgString += `${answers.shape}`
+   
+        let shape;
+        if (answers.shape === 'Circle') {
+            shape = new Circle();
+            svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}" stroke="${answers.textColor}" />`
+        } else if(answers.shape === 'Square') {
+            shape = new Triangle();
+            svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.color}" stroke="${answers.textColor}" />`
+        } else {
+            shape = new Square();
+            svgString += `<rect x="30" y="30" width="240" height="140" fill="${answers.color}" stroke="${answers.textColor}" />`
+        }
+    
+        //set the shape color...
+        svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+        svgString += `</g>`;
+        svgString += `</svg>`
+        
+
+        fs.writeFileSync(fileName, svgString), (err) => {
+            err ? console.log(err) : console.log('logo.svg generated!')
+        }
+}
+
+
+userPrompts();
